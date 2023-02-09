dae8e368a2c22ac94a07a7569d28d6c11450184a
Luoxi Pan <l.panpax@gmail.com>
2023-02-09 11:49:50 +1100

    Fix references to CounterApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// Apps
 import App from './App';
 import CounterApp from './CounterApp'
+// Other
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
